3eb064856d7937afd32f7e49d69dea4f2a86a92d
kimgm08 <tnspzh8@gmail.com>
2020-08-28 01:46:16 +0900

    samjin

---
+++ script/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('#slide').bxSlider({
+        mode:'fade',
+        auto:true,
+        pager:true,
+        pause:5000,
+        speed:700,
+    });
+    /*$('#gift').bxSlider({
+        auto: true,
+        autoControls: false,
+        stopAutoOnClick: false,
+        pager: false,
+        maxSlides:2,
+        responsive:false,
+        slideWidth:585,
+      });*/
+});
